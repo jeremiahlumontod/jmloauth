42816f5dc3e6102d4f6a6ed75f82a480e6c18fb3
jeremiah.lumontod@gmail.com <jeremiah.lumontod@gmail.com>
2017-05-14 17:49:21 +0400

    reverted to spring mvc style

---
+++ src/main/java/org/jml/myeis/model/Person.java
@@ -0,0 +1,14 @@
+package org.jml.myeis.model;
+
+public class Person {
+
+	String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
