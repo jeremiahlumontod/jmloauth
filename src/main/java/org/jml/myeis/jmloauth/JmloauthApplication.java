1756bdf2219ead0224f4f4a65737e360622b23b8
jeremiah.lumontod@gmail.com <jeremiah.lumontod@gmail.com>
2017-05-14 11:31:57 +0400

    first commit

---
+++ src/main/java/org/jml/myeis/jmloauth/JmloauthApplication.java
@@ -0,0 +1,12 @@
+package org.jml.myeis.jmloauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmloauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmloauthApplication.class, args);
+	}
+}
